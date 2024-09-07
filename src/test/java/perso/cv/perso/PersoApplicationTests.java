050cced49935a41d4569747c0cdf26bd9602f8c4
MarionQH <marion.huet49@gmail.com>
2024-09-07 15:23:15 +0200

    mise en place

---
+++ src/test/java/perso/cv/perso/PersoApplicationTests.java
@@ -0,0 +1,13 @@
+package perso.cv.perso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
